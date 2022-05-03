2cbb1054499c74590f1ffbc99f7ecd1e84441745
Vít Novotný <witiko@gmail.com>
2022-05-03 23:21:22 +0200

    Install conda environment in scripts/launch-jupyterlab-at-aura.sh

--- scripts/launch-jupyterlab-at-aura.sh
+++ scripts/launch-jupyterlab-at-aura.sh
@@ -9,7 +9,18 @@ NICENESS=10
 
 # shellcheck disable=SC1090
 source ~/miniconda3/etc/profile.d/conda.sh
-conda activate arqmath3
+
+if ! conda activate arqmath3
+then
+    conda create --name arqmath3 python=3.9
+    conda activate arqmath3
+    conda install -c conda-forge jupyterlab ipywidgets nodejs=16.6.1
+    conda install -c pytorch pytorch cudatoolkit=11.3
+    rm -rf ~/miniconda3/pkgs/ ~/.cache/
+fi
+
+pip install .[all,notebook]
+
 export CUDA_DEVICE_ORDER=PCI_BUS_ID
 export NVIDIA_VISIBLE_DEVICES="$GPUS"
 nice -n "$NICENESS" jupyter-lab --port "$PORT"
